95ae6389dedb7a62d508f7351491b3685571e6f6
Ceareja <94788310+Ceareja@users.noreply.github.com>
2023-08-15 14:55:25 +0800

    added dashboard sidebar

--- dashboard.php
+++ dashboard.php
@@ -10,27 +10,37 @@
     <!--  myCSS -->
     <link rel="stylesheet" href="/appointment-system/CSS/app.css">
     <!-- fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Javascript -->
     <script type="text/javascript" src="/appointment-system/script.js"></script>
+    <!-- font awesome -->
+    <link rel="stylesheet" href="/appointment-system/vendor/fontawesome-free-6.4.2-web/css/all.css">
+    <link rel="stylesheet" href="/appointment-system/vendor/fontawesome-free-6.4.2-web/css/all.css">
     <title>Admin Dashboard</title>
 </head>
 
 <body class="admin-base-font">
     <div class="row">
-        <div class="col-4 admin-leftside-col">
-            <table>
-                <tr>
-                    <td><img class="dashboard-logo" src="/appointment-system/images/logo.png"></td>
-                    <td>Rosario Appointment</td>
-                </tr>
-                <hr>
-                <tr>
+        <div class="col-md-2 col-sm-1 admin-leftside-col sidebar bg-gradient-dark">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+                <img class="dashboard-logo" src="/appointment-system/images/logo.png">
+                <div class="admin-leftside-col-text">Rosario Appointment</div>
+            </a>
+            <hr class="sidebar-divider my-0">
+            <ul class="nav">
+                <li class="nav-item"><a class="nav-link" href="test.php"><i class="fas fa-tachometer-alt me-2"></i><span>Dashboard</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-user me-2"></i><span>Brgy Official</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-users me-2"></i><span>Client</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-user-clock me-2"></i><span>Brgy Official Schedule</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-clipboard me-2"></i><span>Appointment</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="far fa-id-card me-2"></i><span>Profile</span></a></li>
+            </ul>
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"><i class="fa-solid fa-less-than fa-2xs" style="color: #ffffff;"></i></i></button>
+            </div>
 
-                </tr>
-            </table>
         </div>
-        <div class="col-8">
+        <div class="col-10">
 
         </div>
     </div>
